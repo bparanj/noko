fcdd083e561b352018218961c3616b2e56d30bbc
Bala Paranj <bparanj@onekingslane.com>
2016-05-11 16:37:12 -0700

    first commit

---
+++ db/migrate/20160511183300_create_products.rb
@@ -0,0 +1,13 @@
+class CreateProducts < ActiveRecord::Migration[5.0]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.decimal :price
+      t.references :category, foreign_key: true
+      t.text :description
+      t.string :custom_url
+
+      t.timestamps
+    end
+  end
+end
